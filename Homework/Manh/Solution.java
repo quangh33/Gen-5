e61b3693cc3c519a09be1a33b8a78e380ce6b9a3
1997nguyenvanmanh <1997nguyenvanmanh@gmail.com>
2022-03-06 19:59:48 +0700

    check contains dupicate items

---
+++ Homework/Manh/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            set.add(nums[i]);
+        }
+        return set.size() != nums.length;
+    }
+}
