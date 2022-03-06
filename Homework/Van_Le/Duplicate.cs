c81acc5b464cd57a98cca81f6b5d2f666f915fb9
diegovanne <dinhvanle.it@gmail.com>
2022-03-06 22:47:24 +0700

    contains duplicate solution by van le

---
+++ Homework/Van_Le/Duplicate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace ContainDuplicate
+{
+    public static class Duplicate
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            Dictionary<int, int> myMap = new Dictionary<int, int> ();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (myMap.ContainsKey(nums[i]))
+                {
+                    return true;
+                }
+                myMap[nums[i]] = i;
+            }
+            return false;
+        }
+    }
+}
