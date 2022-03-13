7f11007385dbca19dcb74772059ebd2b08015cca
Viet Hoang <ngviethoang0212@gmail.com>
2022-03-13 20:29:41 +0700

    update happy number solution

--- Homework/Hoang_Nguyen/lesson 1/202_happy_number.js
+++ Homework/Hoang_Nguyen/lesson 1/202_happy_number.js
@@ -3,6 +3,38 @@
  * @return {boolean}
  */
 var isHappy = function (n) {
+  function nToSum(n) {
+    let sum = 0;
+    while (n) {
+      sum += Math.pow(n % 10, 2);
+      n = Math.floor(n / 10);
+    }
+    return sum;
+  }
+
+  let slow = n;
+  let fast = n;
+
+  while (fast !== 1) {
+    slow = nToSum(slow);
+    fast = nToSum(fast);
+    if (fast === 1) {
+      break;
+    }
+    fast = nToSum(fast);
+
+    if (slow === fast) {
+      return false;
+    }
+  }
+  return true;
+};
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappyBySet = function (n) {
   let set = new Set();
   while (!set.has(n)) {
     set.add(n);
