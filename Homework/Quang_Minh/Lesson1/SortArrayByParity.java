0628293c9603b91728a4939b25e7aa1f34067a0e
QuangMinh <nquangminh9x@gmail.com>
2022-03-13 15:44:18 +0700

    update as the comments

--- Homework/Quang_Minh/Lesson1/SortArrayByParity.java
+++ Homework/Quang_Minh/Lesson1/SortArrayByParity.java
@@ -3,26 +3,26 @@ class SortArrayByParity {
         int size = nums.length;
         int position = 0;
         for (int i = 0; i < size; i++) {
-            if (nums[i] %2 == 0) {
-                if (position != i) {
-                    nums[i] = nums[position] + nums[i];
-                    nums[position] = nums[i] - nums[position];
-                    nums[i] = nums[i] - nums[position];
-                }
+            if (nums[i] % 2 == 0) {
+                swap(nums, position, i);
                 position++;
             }
         }
         position = size - 1;
-        for (int i = size - 1; i >=0 ; i--){
-            if (nums[i] %2 != 0) {
-                if (position != i) {
-                    nums[i] = nums[position] + nums[i];
-                    nums[position] = nums[i] - nums[position];
-                    nums[i] = nums[i] - nums[position];
-                }
+        for (int i = size - 1; i >= 0; i--) {
+            if (nums[i] % 2 != 0) {
+                swap(nums, position, i);
                 position--;
             }
         }
         return nums;
+    }
+
+    private void swap(int[] input, int i, int j) {
+        if (i != j) {
+            input[i] = input[i] + input[j];
+            input[j] = input[i] - input[j];
+            input[i] = input[i] - input[j];
+        }
     }
 }
--- Homework/Quang_Minh/Lesson1/SortColors.java
+++ Homework/Quang_Minh/Lesson1/SortColors.java
@@ -1,10 +1,9 @@
-class SortColors {
     public void sortColors(int[] nums) {
         int size = nums.length;
         int position = 0;
         // move all 0, 1 to the left
         for (int i = 0; i < size; i++) {
-            if (nums[i] != 2) {
+            if (nums[i] == 0) {
                 if (position != i) {
                     nums[i] = nums[position] + nums[i];
                     nums[position] = nums[i] - nums[position];
@@ -16,7 +15,7 @@ class SortColors {
         // move all 1, 2 tho the right
         position = size - 1;
         for (int i = size - 1; i >=0 ; i--){
-            if (nums[i] != 0) {
+            if (nums[i] == 2) {
                 if (position != i) {
                     nums[i] = nums[position] + nums[i];
                     nums[position] = nums[i] - nums[position];
@@ -24,6 +23,5 @@ class SortColors {
                 }
                 position--;
             }
-        }   
+        }
     }
-}
