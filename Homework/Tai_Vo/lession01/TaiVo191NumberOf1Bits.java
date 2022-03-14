68ea449586d4d0dbd465c37e1062ce5c1c5c7bc3
TaiVT <>
2022-03-14 20:50:58 +0700

    [TaiVo] Completed "191. Number of 1 Bits"

---
+++ Homework/Tai_Vo/lession01/TaiVo191NumberOf1Bits.java
@@ -0,0 +1,28 @@
+package lession01;
+
+/**
+ * @author taivt
+ * @since 2022/03/14 20:37:19
+ */
+public class TaiVo191NumberOf1Bits {
+    public static void main(String[] args) {
+        System.out.println(String.format("%32s", Integer.toBinaryString(11)).replace(' ', '0'));
+        System.out.println(new TaiVo191NumberOf1Bits().hammingWeight(11));
+        System.out.println(String.format("%32s", Integer.toBinaryString(128)).replace(' ', '0'));
+        System.out.println(new TaiVo191NumberOf1Bits().hammingWeight(128));
+        System.out.println(String.format("%32s", Integer.toBinaryString(-3)).replace(' ', '0'));
+        System.out.println(new TaiVo191NumberOf1Bits().hammingWeight(-3));
+    }
+
+    public int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            boolean isRightMostBit1 = (n | 1) == n;
+            if (isRightMostBit1) {
+                count++;
+            }
+            n >>= 1;
+        }
+        return count;
+    }
+}
