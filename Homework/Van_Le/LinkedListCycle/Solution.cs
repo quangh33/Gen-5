b606b462781970fec07f85e70e16ea00528e7da0
diegovanne <dinhvanle.it@gmail.com>
2022-03-14 09:37:15 +0700

    update homework 14-2

---
+++ Homework/Van_Le/LinkedListCycle/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LinkedListCycle
+{
+    public class LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            ListNode slowPointer = head;
+            ListNode fastPointer = head;
+            while (fastPointer != null && fastPointer.next != null)
+            {
+                slowPointer = slowPointer.next;
+                fastPointer = fastPointer.next.next;
+                if (slowPointer == fastPointer) { return true; }
+            }
+            return false;
+        }
+    }
+}
---
+++ Homework/Van_Le/MoveZeroes/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace MoveZeroes
+{
+    public static class MoveZeroes
+    {
+        public static void MoveZeroes(int[] nums)
+        {
+            int left = 0;
+            int right = 0;
+            while (right < nums.Length)
+            {
+
+                if (nums[right] != 0)
+                {
+                    int temp = nums[right];
+                    nums[right] = nums[left];
+                    nums[left] = temp;
+                    left++;
+                }
+                right++;
+            }
+        }
+    }
+}
