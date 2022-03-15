3965a5d881bd6b9b9a684a97d9e052211fd2c597
Nguyen An Khang <nguyenak95@gmail.com>
2022-03-15 22:14:17 +0700

    update implementation hw 1

--- Homework/Khang_Nguyen/is-happy.js
+++ Homework/Khang_Nguyen/is-happy.js
@@ -1,22 +1,51 @@
-/**
+// /**
+//  * @param {number} n
+//  * @return {boolean}
+//  */
+//  var isHappy = function(n) {
+//     const freq = { [n]: 1 }
+//     let clone = n
+//     let result = true
+//     while (clone !== 1) {
+//         clone = clone.toString().split('').reduce((total, digit) => {
+//             const num = parseInt(digit)
+//             return total + num*num
+//         }, 0)
+//         if (freq[clone]) {
+//             result = false
+//             break;
+//         } else {
+//             freq[clone] = 1
+//         }
+//     }
+//     return result
+// };
+i/**
  * @param {number} n
  * @return {boolean}
  */
- var isHappy = function(n) {
-    const freq = { [n]: 1 }
-    let clone = n
-    let result = true
-    while (clone !== 1) {
-        clone = clone.toString().split('').reduce((total, digit) => {
-            const num = parseInt(digit)
-            return total + num*num
-        }, 0)
-        if (freq[clone]) {
-            result = false
-            break;
-        } else {
-            freq[clone] = 1
-        }
-    }
-    return result
+var isHappy = function(n) {
+  let slow = squareDigit(n)
+  let fast = squareDigit(slow)
+
+  while (slow !== fast) {
+      slow = squareDigit(slow)
+      fast = squareDigit(fast)
+      if (fast === slow) break;
+      
+      fast = squareDigit(fast)
+      if (fast === slow) break;
+  }
+  return slow === 1
 };
+
+function squareDigit(n) {
+    let sum = 0;
+    let temp;
+    while (n > 0) {
+        temp = n % 10
+        sum = sum + temp * temp
+        n = Math.floor(n/10)
+    }
+    return sum
+}
--- Homework/Khang_Nguyen/range-sum-query-2d-immutable.js
+++ Homework/Khang_Nguyen/range-sum-query-2d-immutable.js
@@ -8,17 +8,13 @@ var NumMatrix = function(matrix) {
     const rowLen = matrix.length
     const dp = []
 
-    for (let row = 0; row < rowLen; row++) {
+    for (let row = 0; row < rowLen + 1; row++) {
         dp.push([])
-        for (let col = 0; col < colLen; col++) {
-            if (row - 1 >= 0 && col - 1 >= 0) {
-               dp[row][col] = dp[row - 1][col] + dp[row][col - 1] - dp[row - 1][col -1] + matrix[row][col]
-            } else if (row - 1 >= 0) {
-               dp[row][col] = dp[row - 1][col] + matrix[row][col]
-            } else if (col - 1 >= 0) {
-               dp[row][col] = dp[row][col - 1] + matrix[row][col]
+        for (let col = 0; col < colLen + 1; col++) {
+            if (row === 0 || col === 0) {
+                dp[row][col] = 0
             } else {
-               dp[row][col] = matrix[row][col]
+                dp[row][col] = dp[row][col - 1] + dp[row - 1][col] + matrix[row - 1][col - 1] - dp[row - 1][col - 1];
             }
         }
     }
@@ -33,8 +29,5 @@ var NumMatrix = function(matrix) {
  * @return {number}
  */
 NumMatrix.prototype.sumRegion = function(row1, col1, row2, col2) {
-    const upperLeft = (row1 >= 1) && (col1 >= 1) ? this.dp[row1 - 1][col1 -1] : 0 
-    const upperRight = (row1 >= 1) ? this.dp[row1 - 1][col2] : 0 
-    const lowerLeft = (col1 >= 1) ? this.dp[row2][col1 -1] : 0 
-    return this.dp[row2][col2] - upperRight - lowerLeft + upperLeft
+    return this.dp[row2 + 1][col2 + 1] - this.dp[row1][col2 + 1] - this.dp[row2 + 1][col1] + this.dp[row1][col1] 
 };
