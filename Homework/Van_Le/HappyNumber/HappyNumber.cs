b8d66ea08026cd8dd5550d31c147bc13fb4be986
diegovanne <dinhvanle.it@gmail.com>
2022-03-15 10:03:31 +0700

    update to floyd

--- Homework/Van_Le/HappyNumber/HappyNumber.cs
+++ Homework/Van_Le/HappyNumber/HappyNumber.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SingleNumber
+namespace HappyNumber
 {
-    internal class HappyNumber
+    public static class HappyNumber
     {
+        public static bool IsHappy(int n)
+        {
+            int slowRunner = n;
+            int fastRunner = GetNext(n);
+            while (fastRunner != 1 && slowRunner != fastRunner)
+            {
+                slowRunner = GetNext(slowRunner);
+                fastRunner = GetNext(GetNext(fastRunner));
+            }
+            return fastRunner == 1;
+        }
+        private static int GetNext(int n)
+        {
+            int totalSum = 0;
+            while (n > 0)
+            {
+                int d = n % 10;
+                n = n / 10;
+                totalSum += d * d;
+            }
+            return totalSum;
+        }
     }
 }
