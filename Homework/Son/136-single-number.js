9dba835ab0753a1f2528b6057d2c8aef215bed28
Nolan <nolan.nn235@gmail.com>
2022-03-15 11:39:29 +0700

    add new homework

--- Homework/Son/136-single-number.js
+++ Homework/Son/136-single-number.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
  var singleNumber = function(nums) {
-    let numStack = new Set();
+    let numSet = new Set();
     for (let num of nums) {
-        if (!numStack.has(num)) numStack.add(num)
-        else if (numStack.has(num)) numStack.delete(num)
+        if (!numSet.has(num)) numSet.add(num)
+        else if (numSet.has(num)) numSet.delete(num)
     }
-    return numStack.values().next().value;
+    return numSet.values().next().value;
 };
