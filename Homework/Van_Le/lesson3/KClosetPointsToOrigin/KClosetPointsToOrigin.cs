4c617ce140799fa3c4d02feee2bd4ee35d89db37
diegovanne <dinhvanle.it@gmail.com>
2022-03-21 13:47:25 +0700

    update

---
+++ Homework/Van_Le/lesson3/KClosetPointsToOrigin/KClosetPointsToOrigin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace KClosetPointsToOrigin
+{
+    public class KClosetPointsToOrigin
+    {
+        public int[][] KClosest(int[][] points, int k)
+        {
+            return points.OrderBy(p => Math.Sqrt(p[0] * p[0] + p[1] * p[1])).Take(k).ToArray();
+        }
+    }
+}
