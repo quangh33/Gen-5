7a2560e336de2915fd0488c3a789bd55eee5a4d9
Nam Nguyen <nam444479@gmail.com>
2022-03-26 11:47:27 +0700

    resolved comments

--- Homework/Nam_Nguyen/Lesson2/LeetCode_1636_SortArrayByIncreasingFrequency.java
+++ Homework/Nam_Nguyen/Lesson2/LeetCode_1636_SortArrayByIncreasingFrequency.java
@@ -1,6 +1,10 @@
 package Homework.Nam_Nguyen.Lesson2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LeetCode_1636_SortArrayByIncreasingFrequency {
@@ -8,22 +12,25 @@ public class LeetCode_1636_SortArrayByIncreasingFrequency {
     static Map<Integer, Integer> frequencyMap = new HashMap<>();
 
     public static int[] frequencySort(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        // to use built-in sort with custom comparator, I need to pass an list of object,
+        // so I map the array of int (primative) to list of Integer
+        // here I need extra space O(n), n = length of nums
+        // but n in range [1, 100], it doesn't matter.
         for(int num: nums) {
             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0)+1);
+            result.add(num);
         }
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if(frequencyMap.get(nums[i]) > frequencyMap.get(nums[j])) {
-                    swap(i, j, nums);
-                }else if(frequencyMap.get(nums[i]) == frequencyMap.get(nums[j])){
-                    if(nums[i] < nums[j]) {
-                        swap(i, j, nums);
-                    }
+        Collections.sort(result, new Comparator<Integer>() {
+            public int compare(Integer number1, Integer number2) {
+                if(frequencyMap.get(number2) == frequencyMap.get(number1)){
+                    return number2 - number1;
                 }
+                return frequencyMap.get(number1) - frequencyMap.get(number2);
             }
-        }
-        return nums;
+        });
+        // I map our result to array of int to meet the problem's output
+        return result.stream().mapToInt(i -> i).toArray();
     }
 
     public static void swap(int i, int j, int[] nums) {
