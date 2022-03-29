2f416654b9cc23073d94a060bc01905bdd94b624
TaiVT <>
2022-03-29 23:00:11 +0700

    [TaiVo] Completed "1122. Relative Sort Array"

---
+++ Homework/Tai_Vo/lesson05/TaiVo974SubarraySumsDivisibleByK.java
@@ -0,0 +1,18 @@
+package lesson05;
+
+/**
+ * @author taivt
+ * @link https://leetcode.com/problems/subarray-sums-divisible-by-k/
+ * @since 2022/03/28 23:22:56
+ */
+public class TaiVo974SubarraySumsDivisibleByK {
+    public static void main(String[] args) {
+
+    }
+
+    private static class Solution {
+        public int subarraysDivByK(int[] nums, int k) {
+            return 1;
+        }
+    }
+}
