28eeb3549d578c3c3d5032fed83bac4403313c99
TaiVT <>
2022-03-30 22:10:37 +0700

    [TaiVo] Completed "974. Subarray Sums Divisible by K"

--- Homework/Tai_Vo/lesson05/TaiVo974SubarraySumsDivisibleByK.java
+++ Homework/Tai_Vo/lesson05/TaiVo974SubarraySumsDivisibleByK.java
@@ -1,5 +1,8 @@
 package lesson05;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author taivt
  * @link https://leetcode.com/problems/subarray-sums-divisible-by-k/
@@ -7,12 +10,34 @@ package lesson05;
  */
 public class TaiVo974SubarraySumsDivisibleByK {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        System.out.println(solution.subarraysDivByK(new int[]{4, 5, 0, -2, -3, 1}, 5));
+        System.out.println(solution.subarraysDivByK(new int[]{5}, 9));
     }
 
+    // Space Complexity: O(k)
+    // Time Complexity: O(n)
     private static class Solution {
         public int subarraysDivByK(int[] nums, int k) {
-            return 1;
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(0, 1);
+
+            int sumSoFar = 0;
+            int count = 0;
+            for (int num : nums) {
+                sumSoFar = sumSoFar + num;
+                int remainder = sumSoFar % k;
+
+                if (remainder < 0) {
+                    remainder = remainder + k;
+                }
+
+                Integer prevRemainder = map.getOrDefault(remainder, 0);
+                count = count + prevRemainder;
+                map.put(remainder, prevRemainder + 1);
+            }
+
+            return count;
         }
     }
 }
