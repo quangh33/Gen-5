d38cc8ec43f5771e65abf6c46b3b0c3c31ac5fb0
Nam Nguyen <nam444479@gmail.com>
2022-04-03 08:36:36 +0700

    improve Min Stack

--- Homework/Nam_Nguyen/Lesson4/MinStack.java
+++ Homework/Nam_Nguyen/Lesson4/MinStack.java
@@ -1,15 +1,37 @@
 package Homework.Nam_Nguyen.Lesson4;
 
 public class MinStack {
-    int[] stack;
+    public class Pair {
+        private int value;
+        private int minSoFar;
+
+        Pair(int value, int minSoFar){
+            this.value = value;
+            this.minSoFar = minSoFar;
+        }
+    }
+
+    Pair[] stack;
     int capacity = 0;
     public MinStack() {
         // Constraint: At most 3 * 10^4 calls will be made to push, pop, top, and getMin
-        stack = new int[30000];
+        stack = new Pair[30000];
     }
     
+    public int min(int a, int b) {
+        if(a < b)
+            return a;
+        return b;
+    }    
     public void push(int val) {
-        stack[capacity++] = val;
+        int minSoFar;
+        if(capacity > 0)
+            minSoFar = stack[capacity-1].minSoFar;
+        else 
+            minSoFar = Integer.MAX_VALUE;
+        minSoFar = min(minSoFar, val);
+        Pair element = new Pair(val, minSoFar);
+        stack[capacity++] = element;
     }
     
     public void pop() {
@@ -19,18 +41,10 @@ public class MinStack {
     public int top() {
         // Constraint: Methods pop, top and getMin operations will always be called on non-empty stacks.
         // so no need to check stack is empty before get top of stack
-        return stack[capacity-1];
+        return stack[capacity-1].value;
     }
     
     public int getMin() {
-        int k = capacity;
-        int min = stack[--k];
-        while(k >= 0){
-            if(stack[k] < min) {
-                min = stack[k];
-            }
-            k--;
-        }
-        return min;
+        return stack[capacity-1].minSoFar;
     }
 }
