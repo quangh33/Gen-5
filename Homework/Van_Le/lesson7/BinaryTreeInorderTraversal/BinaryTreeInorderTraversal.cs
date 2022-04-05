63c002d7eb2e313242221e55d801c39d6f88abbf
diegovanne <dinhvanle.it@gmail.com>
2022-04-05 16:38:34 +0700

    house robber III and binary tree inorder traversal and binary tree preorder traversal

---
+++ Homework/Van_Le/lesson7/BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeSolutions
+{
+    public class BinaryTreeInorderTraversal
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            List<int> res = new List<int>();
+            TreeNode curr = root;
+            TreeNode pre;
+            while (curr != null)
+            {
+                if (curr.left == null)
+                {
+                    res.Add(curr.val);
+                    curr = curr.right;
+                }
+                else
+                {
+                    pre = curr.left;
+                    while (pre.right != null)
+                    { 
+                        pre = pre.right;
+                    }
+                    pre.right = curr;
+                    TreeNode temp = curr;
+                    curr = curr.left;
+                    temp.left = null;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Homework/Van_Le/lesson7/HouseRobberIII/HouseRobberIII.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCodeSolutions
+{
+    public class HouseRobberIII
+    {
+        Dictionary<TreeNode, int> robResult = new Dictionary<TreeNode, int>();
+        Dictionary<TreeNode, int> notRobResult = new Dictionary<TreeNode, int>();
+
+        public int Helper(TreeNode node, bool parentRobbed)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            if (parentRobbed)
+            {
+                if (robResult.ContainsKey(node))
+                {
+                    return robResult[node];
+                }
+                int result = Helper(node.left, false) + Helper(node.right, false);
+                robResult[node] = result;
+                return result;
+            }
+            else
+            {
+                if (notRobResult.ContainsKey(node))
+                {
+                    return notRobResult[node];
+                }
+                int rob = node.val + Helper(node.left, true) + Helper(node.right, true);
+                int notRob = Helper(node.left, false) + Helper(node.right, false);
+                int result = Math.Max(rob, notRob);
+                notRobResult[node] = result;
+                return result;
+            }
+        }
+
+        public int Rob(TreeNode root)
+        {
+            return Helper(root, false);
+        }
+    }
+}
