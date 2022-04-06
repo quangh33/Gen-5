3639644b94c36832fc7a1b8af23dab7e6f3e8e2b
Nam Nguyen <nam444479@gmail.com>
2022-04-06 09:47:14 +0700

    submitted 4 problems left on homework lesson 7

---
+++ Homework/Nam_Nguyen/Lesson7/LeetCode_94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,52 @@
+package Homework.Nam_Nguyen.Lesson7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode_94_BinaryTreeInorderTraversal {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        List<Integer> result;
+
+        public void DFS(TreeNode node) {
+            if(node == null)    return;
+            DFS(node.left);
+            result.add(node.val);
+            DFS(node.right);
+        }
+
+        public List<Integer> inorderTraversal(TreeNode root) {
+            result = new ArrayList<>();
+            DFS(root);
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode main = new TreeNode();
+        TreeNode n21 = new TreeNode(4);
+        TreeNode n22 = new TreeNode(5);
+        TreeNode n11 = new TreeNode(2, n21, n22);
+        TreeNode n12 = new TreeNode(3);
+        TreeNode root = new TreeNode(1, n11, n12);
+        System.out.println(main.inorderTraversal(root));
+    }
+
+}
