d758ba5d77cbf6bf409ad98bb3453cd4d923e1b7
TaiVT <>
2022-04-06 22:36:07 +0700

    [TaiVo] Updated "543. Diameter of Binary Tree"

---
+++ Homework/Tai_Vo/lesson07/TaiVo543DiameterOfBinaryTree.java
@@ -0,0 +1,64 @@
+package lesson07;
+
+/**
+ * @author taivt
+ * @link https://leetcode.com/problems/diameter-of-binary-tree/
+ * @since 2022/04/06 21:48:01
+ */
+public class TaiVo543DiameterOfBinaryTree {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        TreeNode treeNode = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+        System.out.println(solution.diameterOfBinaryTree(treeNode));
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static class Solution {
+        private int maxDiameter = 0;
+
+        public int diameterOfBinaryTree(TreeNode node) {
+            calculateDiameter(node);
+            return maxDiameter;
+        }
+
+        private int calculateDiameter(TreeNode node) {
+            if (node == null) {
+                return 0;
+            }
+
+            TreeNode left = node.left;
+            TreeNode right = node.right;
+            if (left == null && right == null) {
+                return 0;
+            }
+
+            int maxHeightLeft = 0;
+            if (left != null) {
+                maxHeightLeft = calculateDiameter(left) + 1;
+            }
+            int maxHeightRight = 0;
+            if (right != null) {
+                maxHeightRight = calculateDiameter(right) + 1;
+            }
+            maxDiameter = Math.max(maxDiameter, maxHeightLeft + maxHeightRight);
+            return Math.max(maxHeightLeft, maxHeightRight);
+        }
+    }
+}
