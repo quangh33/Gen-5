4d1b0020ee70cf82e1105c585235856d59f2f65e
TaiVT <>
2022-05-09 02:23:05 +0700

    76. Minimum Window Substring

--- Homework/Tai_Vo/lesson11/TaiVo76MinimumWindowSubstring.java
+++ Homework/Tai_Vo/lesson11/TaiVo76MinimumWindowSubstring.java
@@ -13,35 +13,48 @@ public class TaiVo76MinimumWindowSubstring {
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.minWindow("ADOBECODEBANC", "ABC"));
-//        System.out.println(solution.minWindow("a", "a"));
-//        System.out.println(solution.minWindow("a", "aa"));
+        System.out.println(solution.minWindow("a", "a"));
+        System.out.println(solution.minWindow("a", "aa"));
     }
 
+    // n: s.length, m: t.length
+    // Space Complexity: O(n + m)
+    // Time Complexity: O(n + m)
     private static class Solution {
         public String minWindow(String s, String t) {
-            Map<Character, Integer> freqOfT = new HashMap<>();
+            int n = s.length();
+            int l = 0;
+            int r = 0;
+
+            Map<Character, Integer> dictT = new HashMap<>();
             for (char c : t.toCharArray()) {
-                freqOfT.put(c, freqOfT.getOrDefault(c, 0) + 1);
+                dictT.put(c, dictT.getOrDefault(c, 0) + 1);
             }
 
-            int n = s.length();
-            int l = 0, r = 0;
+            int required = dictT.size();
+            int formed = 0;
             String res = "";
-            Map<Character, Integer> finding = new HashMap<>(freqOfT);
+            Map<Character, Integer> windowFreq = new HashMap<>();
             while (r < n) {
-                if (finding.size() > 0) {
-                    char charAtR = s.charAt(r);
-                    if (finding.containsKey(charAtR)) {
-                        finding.put(charAtR, finding.get(charAtR) - 1);
-                        finding.remove(charAtR, 0);
+                char charAtR = s.charAt(r);
+                windowFreq.put(charAtR, windowFreq.getOrDefault(charAtR, 0) + 1);
+                r++;
+
+                if (dictT.containsKey(charAtR) && dictT.get(charAtR).equals(windowFreq.get(charAtR))) {
+                    formed++;
+                }
+
+                while (formed == required) {
+                    char charAtL = s.charAt(l);
+                    windowFreq.put(charAtL, windowFreq.get(charAtL) - 1);
+                    if (dictT.containsKey(charAtL) && windowFreq.get(charAtL) < dictT.get(charAtL)) {
+                        formed--;
+
+                        String subStr = s.substring(l, r);
+                        if (res.isEmpty() || subStr.length() < res.length()) {
+                            res = subStr;
+                        }
                     }
-                    r++;
-                } else {
-                    String curRes = s.substring(l, r);
-                    if (res.isEmpty() || curRes.length() < res.length()) {
-                        res = curRes;
-                    }
-                    finding = new HashMap<>(freqOfT);
                     l++;
                 }
             }
