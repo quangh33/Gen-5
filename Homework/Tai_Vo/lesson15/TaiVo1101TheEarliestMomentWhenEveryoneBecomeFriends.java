5cc496c63a4f6d6d4730242020853869ab462237
TaiVT <>
2022-05-17 20:49:04 +0700

    1101. The Earliest Moment When Everyone Become Friends

---
+++ Homework/Tai_Vo/lesson15/TaiVo1101TheEarliestMomentWhenEveryoneBecomeFriends.java
@@ -0,0 +1,69 @@
+package lesson15;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author taivt
+ * @link https://leetcode.com/problems/the-earliest-moment-when-everyone-become-friends/
+ * @since 2022/05/14 18:56:34
+ */
+public class TaiVo1101TheEarliestMomentWhenEveryoneBecomeFriends {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.earliestAcq(new int[][]{{20190101, 0, 1}, {20190104, 3, 4}, {20190107, 2, 3}, {20190211, 1, 5}, {20190224, 2, 4}, {20190301, 0, 3}, {20190312, 1, 2}, {20190322, 4, 5}}, 6));
+        System.out.println(solution.earliestAcq(new int[][]{{0, 2, 0}, {1, 0, 1}, {3, 0, 3}, {4, 1, 2}, {7, 3, 1}}, 4));
+        System.out.println(solution.earliestAcq(new int[][]{{9, 3, 0}, {0, 2, 1}, {8, 0, 1}, {1, 3, 2}, {2, 2, 0}, {3, 3, 1}}, 4));
+    }
+
+    private static class Solution {
+
+        private static class DisjointSet {
+            private final int[] parent;
+            private int nOfNodes;
+
+            private DisjointSet(int nOfNodes) {
+                parent = new int[nOfNodes];
+                for (int i = 0; i < parent.length; i++) {
+                    parent[i] = i;
+                }
+                this.nOfNodes = nOfNodes;
+            }
+
+            private int find(int x) {
+                if (x == parent[x]) {
+                    return x;
+                }
+                return parent[x] = find(parent[x]);
+            }
+
+            private int union(int u, int v) {
+                int rootU = find(u);
+                int rootV = find(v);
+                if (rootU == rootV) {
+                    return nOfNodes;
+                } else {
+                    parent[rootU] = rootV;
+                }
+
+                return --nOfNodes;
+            }
+        }
+
+        public int earliestAcq(int[][] logs, int n) {
+            Arrays.sort(logs, Comparator.comparingInt(log -> log[0]));
+            DisjointSet disjointSet = new DisjointSet(n);
+            for (int[] log : logs) {
+                int timestamp = log[0];
+                int x = log[1];
+                int y = log[2];
+                int nOfNodes = disjointSet.union(x, y);
+                if (nOfNodes == 1) {
+                    return timestamp;
+                }
+            }
+            return -1;
+        }
+    }
+}
